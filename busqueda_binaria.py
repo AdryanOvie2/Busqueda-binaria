fd18cf804d3420e050aa2a5628419349763280f4
AdryanOvie2 <adryanovie2@gmail.com>
2023-12-04 12:40:55 +0100

    binary

--- busqueda_binaria.py
+++ busqueda_binaria.py
@@ -1,18 +1,25 @@
 import random
 import time
 
-def busqueda_binaria(lista, objetivo):
-    for i in range(len(lista)):
-        if lista[i] == objetivo:
-            return i
-    return -1
 
-the_black_list = [1, 4, 7, 9, 12]
+def busqueda_binaria(lista, objetivo, l_inferior=None, l_superior=None):
+    if l_inferior is None:
+        l_inferior = 0
+    if l_superior is None:
+        l_superior = len(lista)-1
+    if l_superior < l_inferior:
+        return -1
+    
+    punto_medio = (l_inferior + l_superior) // 2
 
-num = 9
+    if lista[punto_medio] == objetivo:
+        return punto_medio
+    elif objetivo > lista[punto_medio]:
+        return busqueda_binaria(lista, objetivo, punto_medio+1, l_superior)
+    else:
+        return busqueda_binaria(lista, objetivo, l_inferior, punto_medio-1)
+    
 
-if busqueda_binaria(the_black_list, num) > 0:
-    print(f"\nEl valor {num} se encuentra en la lista\n")
-else:
-    print(f"\nEl valor {num} no se encuentra en la lista\n")
-
+if __name__ =='__main__':
+    the_black_list = [1, 4, 7, 9, 12]
+    print(busqueda_binaria(the_black_list, 12))
